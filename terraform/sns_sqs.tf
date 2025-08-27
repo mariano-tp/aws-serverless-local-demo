734ae9033d67ccf5cd9bfcdbbe9d3b8cfd411f40
Mariano T. P. <228102224+mariano-tp@users.noreply.github.com>
2025-08-28 01:42:21 +0200

    Update sns_sqs.tf

--- terraform/sns_sqs.tf
+++ terraform/sns_sqs.tf
@@ -1,33 +1,23 @@
-# Cola creada por CLI (ver job de CI). Aquí solo la LEEMOS.
-data "aws_sqs_queue" "events" {
+resource "aws_sqs_queue" "events" {
   name = "events-queue"
 }
 
-resource "aws_sns_topic" "events" {
-  name = "events-topic"
-}
-
-# Permiso para que SNS pueda enviar a la cola
-resource "aws_sqs_queue_policy" "allow_sns" {
-  queue_url = data.aws_sqs_queue.events.url
+resource "aws_sqs_queue_policy" "allow_s3" {
+  queue_url = aws_sqs_queue.events.id
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Principal = { Service = "sns.amazonaws.com" },
-      Action    = "SQS:SendMessage",
-      Resource  = data.aws_sqs_queue.events.arn,
-      Condition = {
-        ArnEquals = { "aws:SourceArn" = aws_sns_topic.events.arn }
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = { Service = "s3.amazonaws.com" },
+        Action    = "SQS:SendMessage",
+        Resource  = aws_sqs_queue.events.arn,
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_s3_bucket.events.arn
+          }
+        }
       }
-    }]
+    ]
   })
-}
-
-resource "aws_sns_topic_subscription" "sqs_sub" {
-  topic_arn = aws_sns_topic.events.arn
-  protocol  = "sqs"
-  endpoint  = data.aws_sqs_queue.events.arn
-
-  depends_on = [aws_sqs_queue_policy.allow_sns]
 }
