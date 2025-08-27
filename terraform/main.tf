651bbf09324abd4fc42462431019132778b18f67
Mariano T. P. <228102224+mariano-tp@users.noreply.github.com>
2025-08-28 01:42:00 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,9 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.55"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.4"
     }
   }
 }
@@ -19,15 +15,12 @@ provider "aws" {
   s3_use_path_style           = true
   skip_credentials_validation = true
   skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
 
   endpoints {
-    s3            = "http://localhost:4566"
-    dynamodb      = "http://localhost:4566"
-    lambda        = "http://localhost:4566"
-    sns           = "http://localhost:4566"
-    sqs           = "http://localhost:4566"
-    iam           = "http://localhost:4566"
-    logs          = "http://localhost:4566"
-    apigatewayv2  = "http://localhost:4566"
+    s3       = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    sns      = "http://localhost:4566"
   }
 }
